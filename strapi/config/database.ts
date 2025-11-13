54506851ac8fb079339e87823b9599d2aec58076
Uzam Ullah <uzamullahkhan@gmail.com>
2025-11-14 03:15:33 +0500

    Update database config to support SQLite and PostgreSQL

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,13 +1,31 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    sqlite: {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        useNullAsDefault: true,
+      },
     },
-  },
-});
+    postgres: {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        useNullAsDefault: true,
+      },
+    },
+  };
+
+  return connections[client] || connections.sqlite;
+};
