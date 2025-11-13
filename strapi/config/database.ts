93028e9a6e4c241f34ee1f67dacbd9e130f0dce3
Uzam Ullah <uzamullahkhan@gmail.com>
2025-11-14 02:21:53 +0500

    Add database configuration

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -2,13 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env('DATABASE_SSL') === 'true',
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
   },
 });
